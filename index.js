d773a762adb2680f04f306e8a35a9245940c44c4
Mugane Abel <muganeabel@gmail.com>
2024-04-16 23:11:54 +0300

    improvements

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun( sato= "roller-skate"){
+    return `This Saturday, I want to ${sato}!`
+}
+
+function mondayWork (work= "go to the office"){
+    return (`This Monday, I will ${work}.`)
+}
+
+function wrapAdjective (symbol="*"){
+    emphasis= function (par= "a dedicated programmer") {
+        return `You are ${symbol}${par}${symbol}!`
+    }
+}
